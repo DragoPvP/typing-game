2e0e9a4e01ab09acecb844dd6cc387d24ca7fa8a
Hesham-Khan <heshamkhan132@gmail.com>
2021-06-15 01:41:19 +0530

    Half done
    Word match, score, timer added

--- script.js
+++ script.js
@@ -8,29 +8,7 @@ const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
 
-// List of words for game
-const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving'
-];
+const words = [];
 
 // Init word
 let randomWord;
@@ -39,8 +17,76 @@ let randomWord;
 let score = 0;
 
 // Init time
-ket time = 10;
+let time = 10;
 
-function getRandomWord() {
-  return word[Math.floor(Math.random() * words.length)];
+// Focus on text
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// GetRandom Words from API and push to Array
+async function getRandomWord() {
+
+  const res = await fetch('https://random-words-api.vercel.app/word');
+
+  const data = await res.json();
+  return data[Object.keys(data)[0]].word.toLowerCase();
+
 }
+
+getRandomWord();
+
+// Add Word to DOM
+async function addWordToDOM() {
+  randomWord = await getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update Score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    gameOver();
+  }
+}
+
+// Game Over
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time Runout</h1>
+    <p>Your Final Score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+// Event Listeners
+text.addEventListener('input', e => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    // Clear
+    e.target.value = '';
+
+    time += 5;
+
+    updateTime();
+  }
+});
